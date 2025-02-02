acaf8ec6191767ada4d0f0cf2ac1a68f415e6b15
KarthikDevadiga <kdevadiga497@gmail.com>
2025-02-02 18:01:29 +0530

    added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,30 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+const route = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      }, // loader : provideing a loader
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/CreateOrder", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order />, loader: orderLoader },
+    ],
+  },
+]);
+
 export default function App() {
-  return <h1>Hello Vite</h1>;
+  return <RouterProvider router={route} />;
 }
